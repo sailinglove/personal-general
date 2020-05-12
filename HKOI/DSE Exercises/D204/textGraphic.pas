6b2ad731b1f8886f7d07cda3f454df0d2f24e69d
SailorWontSink <xiang62917989@163.com>
2020-05-12 15:13:00 +0800

    D204 v.1 completed

--- HKOI/DSE Exercises/D204/textGraphic.pas
+++ HKOI/DSE Exercises/D204/textGraphic.pas
@@ -20,4 +20,19 @@ begin
       WriteLn('#');
     end;
   end;
+
+  for i := (input div 2) downto 1 do
+  begin
+    for j := ((input div 2) + 1 - i) downto 1 do
+      Write(' ');
+    Write('#');
+    if i = 1 then
+      WriteLn
+    else
+    begin
+      for j := (i * 2 - 3) downto 1 do
+        Write(' ');
+      WriteLn('#');
+    end;
+  end;
 end.
