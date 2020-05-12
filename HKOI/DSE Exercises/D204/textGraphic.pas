43de563ae3752363d12f7d286e643d82611025a3
SailorWontSink <xiang62917989@163.com>
2020-05-12 15:04:27 +0800

    D204 commit 1, first half done

--- HKOI/DSE Exercises/D204/textGraphic.pas
+++ HKOI/DSE Exercises/D204/textGraphic.pas
@@ -1,14 +1,23 @@
 program textGraphic;
 
 var
-  input : Integer;
+  i, j, input : Integer;
 
 begin
   ReadLn(input);
 
   for i := 1 to ((input div 2) + 1) do
   begin
-    for j := 1 to 
-    Write(' ');
-  WriteLn('#');
+    for j := 1 to ((input div 2) + 1 - i) do
+      Write(' ');
+    Write('#');
+    if i = 1 then
+      WriteLn
+    else
+    begin
+      for j := 1 to (i * 2 - 3) do
+        Write(' ');
+      WriteLn('#');
+    end;
+  end;
 end.
