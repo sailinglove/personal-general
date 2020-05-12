6a9ee487059acbdacbcd2e19eeb6c1b9f3e8f81d
SailorWontSink <xiang62917989@163.com>
2020-05-12 14:49:26 +0800

    HKOI\DSE\D204 added

---
+++ HKOI/DSE Exercises/D204/textGraphic.pas
@@ -0,0 +1,14 @@
+program textGraphic;
+
+var
+  input : Integer;
+
+begin
+  ReadLn(input);
+
+  for i := 1 to ((input div 2) + 1) do
+  begin
+    for j := 1 to 
+    Write(' ');
+  WriteLn('#');
+end.
