aa575fba06c2a85815233bb3ef9d5dd64b1828ed
SailorWontSink <xiang62917989@163.com>
2020-10-17 11:35:47 +0800

    PKU Intro2Computation HW 6-1 completed

---
+++ PKU/Introduction to Calculation A/Homework/6-Phrase_1_Coding_Practice_(4)/6-1-out_continue_prime.cpp
@@ -0,0 +1,32 @@
+ #include<iostream>
+ using namespace std;
+
+bool is_prime(int n) {
+    bool flag = true;
+    for (int i = 2; i <= (n / 2 + 1); i++) {
+        if ((n % i) == 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+
+int main() {
+    int m = 0, k = 0, count = 0;
+    cin >> m >> k;
+    bool flag = false;
+    for (int i = 1; count < k; i++) {
+        if (is_prime(m + i)) {
+            if (!flag) {
+                cout << (m + i);
+                flag = true;
+            } else {
+                cout << ' ' << (m + i);
+            }
+            count++;
+        }
+    }
+
+    return 0;
+}
